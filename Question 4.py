4a78d402d50f560975568c8fa3e490f121f964eb
IAmMADMAX <75090307+Nilanjit07@users.noreply.github.com>
2022-04-10 21:59:42 +0530

    Create Question 4.py

---
+++ Question 4.py
@@ -0,0 +1,28 @@
+
+
+//Q1. Use the correct syntax to print the first item in the fruits tuple.
+
+
+fruits = ("apple", "banana", "cherry")
+print(fruits[0])
+
+
+
+//Q2.Use the correct syntax to print the number of items in the fruits tuple.
+
+
+fruits = ("apple", "banana", "cherry")
+print(len(fruits))
+
+//Q3.Use negative indexing to print the last item in the tuple.
+
+
+fruits = ("apple", "banana", "cherry")
+print(fruits[-1])
+
+//Q4. Use a range of indexes to print the third, fourth, and fifth item in the tuple.
+
+
+fruits = ("apple", "banana", "cherry", "orange", "kiwi", "melon", "mango")
+print(fruits[2:5])
+
